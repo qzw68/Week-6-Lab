4d5e1737c029aebe9d9189386f6fccfbdff47a8c
19jinjinwu <71570477+19jinjinwu@users.noreply.github.com>
2020-10-04 18:12:18 -0400

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,3 +1,5 @@
+import random 
+
 numberOfMember = input("How many members in your group: ")
 nameList = []
 majorList = []
@@ -9,3 +11,16 @@ for i in range(int(numberOfMember)):
 
 for name,major in zip(nameList,majorList):
   print(name+"'s Major is: "+major)
+
+guess = 0
+number = random.randint(1, 10)
+while guess < 5:
+  user_number = int(input("Guess a number I'm thinking of between 1 and 10"))
+  guess += 1
+  if user_number < number: 
+    print("Too low! Try again.")
+  elif user_number > number:
+    print("Too high! Try again.")
+  else:
+    print("Congratulations! You guessed correctly. +10 Bonus points on next test.")
+    break
